89411192a240c3de44f17b0b219304cbba7a3c51
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2021-06-25 16:06:56 +0530

    Update index.jsx

--- src/components/section-experience/index.jsx
+++ src/components/section-experience/index.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
+import React from 'react';
 
-// import Section from '../section';
-// import SummaryItem from '../summary-item';
+import Section from '../section';
+import SummaryItem from '../summary-item';
 
-// const SectionExperience = ({ experience }) => {
-//   if (!experience.length) return null;
+const SectionExperience = ({ experience }) => {
+  if (!experience.length) return null;
 
-//   return (
-//     <Section title="Education">
-//       {experience.map((item) => (
-//         <SummaryItem
-//           key={item.name}
-//           name={item.name}
-//           description={item.description}
-//           link={item.link}
-//         />
-//       ))}
-//     </Section>
-//   );
-// };
+  return (
+    <Section title="Education">
+      {experience.map((item) => (
+        <SummaryItem
+          key={item.name}
+          name={item.name}
+          description={item.description}
+          link={item.link}
+        />
+      ))}
+    </Section>
+  );
+};
 
-// export default SectionExperience;
+export default SectionExperience;
