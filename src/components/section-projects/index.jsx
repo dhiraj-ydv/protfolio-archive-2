feac12134d32d776442b6ed50964e6fcf04822ce
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2021-06-25 15:49:08 +0530

    Update index.jsx

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -7,15 +7,20 @@ const SectionProjects = ({ projects }) => {
   if (!projects.length) return null;
 
   return (
-    <Section title="Projects">
-      {projects.map((project) => (
+    <Section title="Resume">
+
+<div class="col-4 col-6-medium col-12-small">
+											<img src="images/pic01.jpg" class="image fit" alt=""></img>
+
+
+      {/* {projects.map((project) => (
         <SummaryItem
           key={project.name}
           name={project.name}
           description={project.description}
           link={project.link}
         />
-      ))}
+      ))} */}
     </Section>
   );
 };
